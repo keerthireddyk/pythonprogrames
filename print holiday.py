f5e430e8696719aa1543850c8e1561d48336d885
keerthireddyk <42573134+keerthireddyk@users.noreply.github.com>
2018-11-28 12:56:09 +0530

    Create print holiday.py

---
+++ print holiday.py
@@ -0,0 +1,8 @@
+s=str(input())
+if (s=='monday' or s=='tuesday' or s=='wednesday' or s=='thursday' or s=='friday'):
+        print("no")
+elif (s=='sunday' or s=='saturday'):
+    print("yes")
+else:
+    print("invalid")
+    
