0d9999d182522f18404b54c1d3b500f09e0624ea
keerthireddyk <42573134+keerthireddyk@users.noreply.github.com>
2018-12-07 11:14:07 +0530

    Update array after doing right shift.py

--- array after doing right shift.py
+++ array after doing right shift.py
@@ -1,10 +1,10 @@
-k1,n=map(int,input().split())
-k=input().split()
-while(n>0):
-    k.insert(0,k.pop())
-    n=n-1
-for i in range(0,len(k)):
-    if(i!=len(k)-1):
-        print(k[i],end=(" "))
+o,p=map(int,input().split())
+e=input().split()
+while(p>0):
+    e.insert(0,e.pop())
+    p=p-1
+for i in range(0,len(e)):
+    if(i!=len(e)-1):
+        print(e[i],end=(" "))
     else:
-        print(k[i])
+        print(e[i])
